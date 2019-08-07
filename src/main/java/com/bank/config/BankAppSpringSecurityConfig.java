e7a23eef8aabe5f8c88def948324ea9d85f62c09
shiva508 <dasarishiva1@gamil.com>
2019-08-07 13:45:44 +0530

    sdfs

--- src/main/java/com/bank/config/BankAppSpringSecurityConfig.java
+++ src/main/java/com/bank/config/BankAppSpringSecurityConfig.java
@@ -4,15 +4,18 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User.UserBuilder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 
 @Configuration
 @EnableWebSecurity
 public class BankAppSpringSecurityConfig extends WebSecurityConfigurerAdapter {
 @Override
 protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+	UserDetails user1=(UserDetails) User.builder().username("shiva").password("{noop}shiva").roles("EMP");
 	auth.inMemoryAuthentication()
-	.withUser("Shiva").password("shiva").roles("EMP");
+	.withUser("Shiva").password("{noop}shiva").roles("EMP");
 
 }
+
 }

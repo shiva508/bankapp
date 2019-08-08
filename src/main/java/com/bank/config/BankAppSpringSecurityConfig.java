721a610f4730a189ef05324a5092fab8e100a7b4
shiva508 <dasarishiva1@gamil.com>
2019-08-08 19:48:46 +0530

    cdzsvf

--- src/main/java/com/bank/config/BankAppSpringSecurityConfig.java
+++ src/main/java/com/bank/config/BankAppSpringSecurityConfig.java
@@ -20,30 +20,39 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class BankAppSpringSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	private DataSource dataSource;
-	@Override
-	
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth .jdbcAuthentication() .dataSource(dataSource);
-	/*	  auth .jdbcAuthentication() .dataSource(dataSource)
-		  .usersByUsernameQuery("select username,password, enabled from users where username=?"
-		  )
-		  .authoritiesByUsernameQuery("select username, role from user_roles where username=?"
-		  ).passwordEncoder(new BCryptPasswordEncoder());
-		 */
-		
-		/*
-		 * auth.inMemoryAuthentication().withUser("shiva").password("{noop}shiva").roles
-		 * ("Employee");
-		 * auth.inMemoryAuthentication().withUser("shivad").password("{noop}shivad").
-		 * roles("Admin");
-		 */
-		 
 
+	@Autowired
+	public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
+		auth.jdbcAuthentication().dataSource(dataSource)
+				.usersByUsernameQuery("select username,password, enabled from users where username=?")
+				.authoritiesByUsernameQuery("select username, role from user_roles where username=?");
 	}
+
+	/*
+	 * @Override protected void configure(AuthenticationManagerBuilder auth) throws
+	 * Exception { auth .jdbcAuthentication() .dataSource(dataSource); auth
+	 * .jdbcAuthentication() .dataSource(dataSource)
+	 * .usersByUsernameQuery("select username,password, enabled from users where username=?"
+	 * )
+	 * .authoritiesByUsernameQuery("select username, role from user_roles where username=?"
+	 * );
+	 * 
+	 * 
+	 * 
+	 * auth.inMemoryAuthentication().withUser("shiva").password("{noop}shiva").roles
+	 * ("Employee");
+	 * auth.inMemoryAuthentication().withUser("shivad").password("{noop}shivad").
+	 * roles("Admin");
+	 * 
+	 * 
+	 * 
+	 * }
+	 */
 	@Bean
 	public PasswordEncoder passwordEncoder() {
-	    return new  BCryptPasswordEncoder();
+		return new BCryptPasswordEncoder();
 	}
+
 	@Override
 	public void configure(WebSecurity web) throws Exception {
 		web.ignoring().antMatchers("/components/**");
@@ -51,17 +60,9 @@ public class BankAppSpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests()
-		.anyRequest()
-		.authenticated()
-		.and()
-		.formLogin()
-		.loginPage("/customlogin")
-		.loginProcessingUrl("/authenicatuser")
-		.permitAll()
-		.and()
-		.logout()
-		.permitAll().and().exceptionHandling().accessDeniedPage("/403");
+		http.authorizeRequests().anyRequest().authenticated().and().formLogin().loginPage("/customlogin")
+				.loginProcessingUrl("/authenicatuser").usernameParameter("username").passwordParameter("password")
+				.permitAll().and().logout().permitAll().and().exceptionHandling().accessDeniedPage("/403");
 	}
 
 }
